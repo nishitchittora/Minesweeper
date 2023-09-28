5f40e6c2603e35ae58980f8897cc21e03da84cea
Nishit Chittora <nishitchittora@Nishits-MacBook-Pro-2.local>
2023-09-28 15:34:15 +0530

    Grid added

--- rn_ts_app/App.tsx
+++ rn_ts_app/App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Grid from "./src/components/Grid";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	console.log("ASDASD");
+	return (
+		<View style={styles.container}>
+			<Text>Helloasdasd</Text>
+			<Grid n={10} m={10} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
